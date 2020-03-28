f817d9dda46538a7f5832587e79584d6a269e096
Adeniji Oluwagbemiga Daniel <adenijioluwagbemigadaniel@gmail.com>
2020-03-28 09:30:04 +0100

    Update ADENIJI OLUWAGBEMIGA DANIEL - PROJECT REPORT - EMPLOYEE ATTRITION .py

--- ADENIJI OLUWAGBEMIGA DANIEL - PROJECT REPORT - EMPLOYEE ATTRITION .py
+++ ADENIJI OLUWAGBEMIGA DANIEL - PROJECT REPORT - EMPLOYEE ATTRITION .py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 # coding: utf-8
-
+#----------------------------------------------------------------------------------
+#Company X has an issue with employee attrition. 
+#The Company needs a lasting analytical solution to diagnose the causes and predict the future effects.
+#----------------------------------------------------------------------------------
 # In[ ]:
 
 
